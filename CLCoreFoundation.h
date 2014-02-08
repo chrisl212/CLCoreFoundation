3b790416eda4193ad2f0d1d7ea87f55c36dfa875
chrisl212 <chrisloonam@icloud.com>
2014-02-08 16:23:51 -0500

    Initial

---
+++ CLCoreFoundation.h
@@ -0,0 +1,14 @@
+//
+//  CLCoreFoundation.h
+//  CLCoreFoundation
+//
+//  Created by Chris on 2/8/14.
+//  Copyright (c) 2014 A and C Studios. All rights reserved.
+//
+
+#ifndef CLCoreFoundation_CLCoreFoundation_h
+#define CLCoreFoundation_CLCoreFoundation_h
+
+
+
+#endif
